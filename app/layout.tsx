afab1cc5b3e7c03e465f5ca8f8b93902290e015a
mrtalukdar <buduman209@gmail.com>
2025-12-20 04:36:11 +0530

    somehow something changed the layout providers back tosession provider. Was causing build failure for nextauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
-
-import { SessionProvider } from "next-auth/react";
+import { Providers } from "./providers";
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -23,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans antialiased`}>
-        <SessionProvider>{children}</SessionProvider>
+        <Providers>{children}</Providers>
         <Analytics />
       </body>
     </html>
